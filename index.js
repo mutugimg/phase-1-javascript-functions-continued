b35749dfccd8e3d4d171834b23aa8c3f8bb8c7ea
Mark Mutugi <mutugimg@gmail.com>
2023-07-19 21:07:13 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+}
+function mondayWork(activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`
+}
+function wrapAdjective(string="*"){
+    let innerFunction = function (msg="special"){
+        return `You are ${string}${msg}${string}!`
+    }
+    return innerFunction
+}
+console.log (wrapAdjective("*")("a dedicated programmer"))
